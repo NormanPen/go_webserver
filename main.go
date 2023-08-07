735778c7809573b6cac721ea8e5f5c96eee285b4
Norman Pendzich <73986750+NormanPen@users.noreply.github.com>
2023-08-07 19:52:39 +0200

    Webserver funktioniert

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+)
+
+func main() {
+	// Statische Dateien aus dem Build-Ordner deiner React-Anwendung servieren
+	fs := http.FileServer(http.Dir(path.Join(".", "web")))
+	http.Handle("/", fs)
+
+	port := 8181
+	fmt.Printf("Server läuft auf Port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
